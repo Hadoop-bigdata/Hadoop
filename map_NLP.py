f3444108564a37b99c5ee40fb7ae19e1382a6778
Weiyan Wu <32626769+weiyanwuda@users.noreply.github.com>
2017-12-13 15:08:41 -0500

    Update map_NLP.py

--- map_NLP.py
+++ map_NLP.py
@@ -6,14 +6,31 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def mapper():
     
+    # initialize sentiment analyzer
     analyzer = SentimentIntensityAnalyzer()
 
+    # transfer each comment to numeric data
     for line in sys.stdin:
+        
+        # transfer string to python dictionary
         data = eval(line.strip())
+        
+        # get comment rate
         rate = data['overall']
-        summary = data['reviewText']
-        #summary = data['result']
+        
+        # get comment
+        #summary = data['reviewText']
+        summary = data['summary']
+        
+        # use sentiment analyzer to transfer comment to numeric data
         vs = analyzer.polarity_scores(summary)
+        
+        # add rate to ditonary
         vs['rate'] = rate
+        
+        # add reviewer ID as key
+        vs['account'] = data['reviewerID']
+        
+        # pop new numeric data to reducer
         print(vs)
 mapper()
